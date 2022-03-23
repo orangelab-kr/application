2c3a3b0d943bf4ee5649ebd9372f1f9c209e602a
bakjonghun <bakjonghun@hikick.kr>
2022-03-23 02:19:26 -0400

    add: constants & models

---
+++ src/models/navigation.ts
@@ -0,0 +1,12 @@
+export type RootNavigatorRouteParams = {
+  Splash: undefined;
+  Start: undefined;
+  Auth: undefined;
+};
+
+export type AuthNavigatorRouteParams = {
+  SignupName: undefined;
+  SignupBirthday: undefined;
+  SignupTerms: undefined;
+  Verify: undefined;
+};
