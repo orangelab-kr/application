c0d425ec3f3521619e7b0500443d7459435d6450
bakjonghun <bakjonghun@hikick.kr>
2022-03-28 19:54:45 -0400

    add: code push

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
+import CodePush from 'react-native-code-push';
 import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
 import {App} from './src/App';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => CodePush(App));
