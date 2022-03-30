582e9360b14229686943ddc75034cc3f88037378
bakjonghun <bakjonghun@hikick.kr>
2022-03-29 20:09:17 -0400

    add: naver map

--- ios/hikickTests/hikickTests.m
+++ ios/hikickTests/hikickTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface hikick_applicationTests : XCTestCase
+@interface hikickTests : XCTestCase
 
 @end
 
-@implementation hikick_applicationTests
+@implementation hikickTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
