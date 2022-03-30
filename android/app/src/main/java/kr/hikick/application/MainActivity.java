f0cf536ba4ceaef8a7b86f0efd6825a78d51902e
bakjonghun <bakjonghun@hikick.kr>
2022-03-29 20:09:33 -0400

    refactor: rename package name

--- android/app/src/main/java/kr/hikick/application/MainActivity.java
+++ android/app/src/main/java/kr/hikick/application/MainActivity.java
@@ -1,4 +1,4 @@
-package com.hikick_application;
+package kr.hikick.application;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "hikick_application";
+    return "hikick";
   }
 }
--- android/app/src/main/java/kr/hikick/application/MainApplication.java
+++ android/app/src/main/java/kr/hikick/application/MainApplication.java
@@ -1,4 +1,4 @@
-package com.hikick_application;
+package kr.hikick.application;
 
 import android.app.Application;
 import android.content.Context;
@@ -68,7 +68,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.hikick_application.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("kr.hikick.application.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
