72519327ec08d982027d7eb95ef921431f3bfd40
bakjonghun <bakjonghun@hikick.kr>
2022-04-08 22:37:12 -0400

    add: start button

---
+++ src/components/main/home/sheet/MainHomeSheetStartButton.tsx
@@ -0,0 +1,29 @@
+import {faBolt} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import React from 'react';
+import {Text} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import styled from 'styled-components/native';
+import {screenHeight} from '../../../../constants/screenSize';
+
+export const MainHomeSheetStartButton: React.FC = () => {
+  return (
+    <Button>
+      <FontAwesomeIcon icon={faBolt} color="#fff" />
+      <ButtonText>시작</ButtonText>
+    </Button>
+  );
+};
+
+const Button = styled(TouchableOpacity)`
+  background-color: #000;
+  border-radius: 10px;
+  padding: 15px;
+  flex-direction: row;
+`;
+
+const ButtonText = styled(Text)`
+  color: #fff;
+  font-size: ${screenHeight / 48}px;
+  margin-left: 6px;
+`;
