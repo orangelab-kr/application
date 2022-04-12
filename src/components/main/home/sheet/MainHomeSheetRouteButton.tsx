d52dc3e23dd4b502d3006956edf221738fa02b47
bakjonghun <bakjonghun@hikick.kr>
2022-04-12 17:40:43 -0400

    add: 길 안내 features

--- src/components/main/home/sheet/MainHomeSheetRouteButton.tsx
+++ src/components/main/home/sheet/MainHomeSheetRouteButton.tsx
@@ -1,16 +1,46 @@
-import {faBolt, faMap} from '@fortawesome/free-solid-svg-icons';
+import {faMap} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import React from 'react';
 import {Text} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {showLocation} from 'react-native-map-link';
 import styled from 'styled-components/native';
+import {RideKickboard} from '../../../../api/ride';
 import {screenHeight} from '../../../../constants/screenSize';
+import {useGeolocation} from '../../../../hooks/useGeolocation';
+import {HookResultValue} from '../../../../models/hookResult';
 
-export const MainHomeSheetRouteButton: React.FC = () => {
+interface MainHomeSheetRouteButtonProps {
+  kickboard?: HookResultValue<RideKickboard>;
+}
+
+export const MainHomeSheetRouteButton: React.FC<
+  MainHomeSheetRouteButtonProps
+> = ({kickboard}) => {
+  const [location] = useGeolocation();
+  if (!kickboard || !location) return <></>;
+  const {latitude, longitude} = kickboard.status.gps;
+  const onPress = () => {
+    showLocation({
+      latitude: latitude,
+      longitude: longitude,
+      sourceLatitude: location.latitude,
+      sourceLongitude: location.longitude,
+      title: kickboard.kickboardCode,
+      dialogTitle: kickboard.kickboardCode,
+      dialogMessage: '어떤 지도로 길 안내를 받으시겠습니까?',
+      naverCallerName: 'kr.hikick.application',
+      alwaysIncludeGoogle: true,
+      googleForceLatLon: true,
+      cancelText: '취소',
+      directionsMode: 'walk',
+    });
+  };
+
   return (
-    <Button>
+    <Button onPress={onPress}>
       <FontAwesomeIcon icon={faMap} color="#000" />
-      <ButtonText>길 찾기</ButtonText>
+      <ButtonText>길 안내</ButtonText>
     </Button>
   );
 };
