1bdf194468f1490c563ee53c0c53c9f94f681bb6
bakjonghun <bakjonghun@hikick.kr>
2022-04-14 22:58:51 -0400

    add: use gestureHandlerRootHOC for android

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
 import {AppRegistry} from 'react-native';
 import CodePush from 'react-native-code-push';
 import 'react-native-gesture-handler';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 import {name as appName} from './app.json';
 import {App} from './src/App';
 
-AppRegistry.registerComponent(appName, () => CodePush(App));
+AppRegistry.registerComponent(appName, () =>
+  CodePush(gestureHandlerRootHOC(App)),
+);
