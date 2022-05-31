f8b9e69f69c4e46e587d3f379d0db449fe75b468
bakjonghun <bakjonghun@hikick.kr>
2022-05-31 16:02:58 -0400

    add: coupons pagination

--- src/components/coupon/flatlist.tsx
+++ src/components/coupon/flatlist.tsx
@@ -1,10 +1,15 @@
 import {useNavigation} from '@react-navigation/native';
+import _ from 'lodash';
 import React, {useEffect, useRef, useState} from 'react';
-import {Text, View} from 'react-native';
+import {ActivityIndicator, Text, View} from 'react-native';
 import DraggableFlatList from 'react-native-draggable-flatlist';
 import {Notifier, NotifierComponents} from 'react-native-notifier';
 import styled from 'styled-components/native';
-import {PaymentsClient, PaymentsCoupon} from '../../api/payments';
+import {
+  PaymentsClient,
+  PaymentsCoupon,
+  RequestPaymentsGetCoupons,
+} from '../../api/payments';
 import {screenWidth} from '../../constants/screenSize';
 import {CouponItem} from './item';
 
@@ -15,17 +20,34 @@ export interface CouponFlatlistProps {
 export const CouponFlatlist: React.FC<CouponFlatlistProps> = ({onSelect}) => {
   const navigation = useNavigation();
   const [coupons, setCoupons] = useState<PaymentsCoupon[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [total, setTotal] = useState<number>();
   const itemRefs = useRef(new Map());
+  const [params, setParams] = useState<RequestPaymentsGetCoupons>({
+    skip: 0,
+    take: 10,
+  });
 
-  const loadCoupons = () =>
-    PaymentsClient.getCoupons({}).then(({coupons}) => setCoupons(coupons));
+  const loadCoupons = async () => {
+    try {
+      if (loading) return;
+      setLoading(true);
+      const {coupons, total} = await PaymentsClient.getCoupons(params);
+      setCoupons(c => [..._.pullAllBy(c, coupons, 'couponId'), ...coupons]);
+      setTotal(total);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
     loadCoupons();
+  }, [params]);
+
+  useEffect(() => {
     const unsubscribe = navigation.addListener('focus', loadCoupons);
     return unsubscribe;
   }, []);
-
   const onDelete = (coupon: PaymentsCoupon) => async () => {
     const {couponId, couponGroup} = coupon;
     Notifier.showNotification({
@@ -37,15 +59,38 @@ export const CouponFlatlist: React.FC<CouponFlatlistProps> = ({onSelect}) => {
       },
     });
 
-    await PaymentsClient.deleteCoupon(couponId).then(loadCoupons);
+    await PaymentsClient.deleteCoupon(couponId);
+    setCoupons(coupons => coupons.filter(c => c.couponId !== couponId));
+  };
+
+  const onEndReached = ({distanceFromEnd}: {distanceFromEnd: number}) => {
+    if (distanceFromEnd < 0) return;
+    if (loading) return;
+    if (!total || total <= coupons.length) return;
+    setParams(params => {
+      let {take, skip} = params;
+      if (!skip) skip = 0;
+      if (!take) take = 10;
+      skip += take;
+      return {take, skip};
+    });
   };
 
   const onPress = (coupon: PaymentsCoupon) => () =>
     onSelect && onSelect(coupon);
-  return coupons.length > 0 ? (
+  return (
     <DraggableFlatList
+      bounces
       data={coupons}
+      onEndReached={onEndReached}
+      onEndReachedThreshold={0.6}
       keyExtractor={coupon => coupon.couponId}
+      ListFooterComponent={loading ? <ActivityIndicator size="large" /> : <></>}
+      ListEmptyComponent={
+        <NoCouponContainer>
+          <NoCoupon>🤨 아직 쿠폰이 없습니다.</NoCoupon>
+        </NoCouponContainer>
+      }
       renderItem={props => (
         <CouponItem
           {...props}
@@ -55,10 +100,6 @@ export const CouponFlatlist: React.FC<CouponFlatlistProps> = ({onSelect}) => {
         />
       )}
     />
-  ) : (
-    <NoCouponContainer>
-      <NoCoupon>🤨 아직 쿠폰이 없습니다.</NoCoupon>
-    </NoCouponContainer>
   );
 };
 
@@ -74,7 +115,7 @@ const NoCouponContainer = styled(View)`
   border-radius: 8px;
   background-color: #fff;
   margin-top: ${screenWidth * 0.05}px;
-  margin: 10px 0;
+  margin: 10px 5px;
   shadow-color: #999;
   shadow-opacity: 1;
   shadow-radius: 6px;
