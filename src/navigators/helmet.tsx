f0254e89953c828051464bfac148b4390413ae10
bakjonghun <bakjonghun@hikick.kr>
2022-06-10 21:50:47 -0400

    add: helmet return page

--- src/navigators/helmet.tsx
+++ src/navigators/helmet.tsx
@@ -2,6 +2,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {HelmetNavigatorRouteParams} from '../models/navigation';
 import {HelmetBorrow} from '../screens/helmet/borrow';
+import {HelmetReturn} from '../screens/helmet/return';
 import {HelmetTerms} from '../screens/helmet/terms';
 
 export const HelmetNavigator: React.FC = () => {
@@ -13,6 +14,7 @@ export const HelmetNavigator: React.FC = () => {
       screenOptions={{headerShown: false}}>
       <Stack.Screen name="Terms" component={HelmetTerms} />
       <Stack.Screen name="Borrow" component={HelmetBorrow} />
+      <Stack.Screen name="Return" component={HelmetReturn} />
     </Stack.Navigator>
   );
 };
--- src/screens/helmet/return.tsx
+++ src/screens/helmet/return.tsx
@@ -1,29 +1,23 @@
 import {useNavigation} from '@react-navigation/native';
-import AnimatedLottieView, {AnimationObject} from 'lottie-react-native';
+import AnimatedLottieView from 'lottie-react-native';
 import React, {useState} from 'react';
 import {Alert, SafeAreaView, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 import {BottomButton} from '../../components/BottomButton';
 import {Depth} from '../../components/Depth';
 import {screenHeight} from '../../constants/screenSize';
 import {useInterval} from '../../hooks/useInterval';
+import {ConnectStatus, icons} from './borrow';
 
-export type ConnectStatus = 'request' | 'searching' | 'pairing' | 'completed';
-export const icons: {[key in ConnectStatus]: AnimationObject} = {
-  request: require('../../assets/lotties/86974-registration.json'),
-  searching: require('../../assets/lotties/28925-wait-to-connect.json'),
-  pairing: require('../../assets/lotties/95717-connect-onekey-by-bluetooth.json'),
-  completed: require('../../assets/lotties/97163-upload-complete.json'),
-};
-
-const messages: {[key in ConnectStatus]: string} = {
+export const messages: {[key in ConnectStatus]: string} = {
   request: '서버에 요청 중...',
   searching: '주변에 헬멧을 찾는 중...',
-  pairing: '헬멧을 해제하는 중...',
+  pairing: '잠금을 기다리는 중...',
   completed: '완료되었습니다.',
 };
 
-export const HelmetBorrow: React.FC = () => {
+export const HelmetReturn: React.FC = () => {
   const navigation = useNavigation();
   const [status, setStatus] = useState<ConnectStatus>('request');
   useInterval(
@@ -40,11 +34,24 @@ export const HelmetBorrow: React.FC = () => {
   const onBack = () => {
     const onPress = () => navigation.goBack();
     Alert.alert(
-      '헬멧과 통신하는 중...',
-      '헬멧 대여를 취소하시겠습니까?',
+      '라이드를 종료하는 중...',
+      '이어서 라이드를 진행하시겠습니까?',
       [
         {text: '아니요', style: 'cancel'},
         {text: '네, 취소합니다', onPress},
+      ],
+      {cancelable: true},
+    );
+  };
+
+  const onLost = () => {
+    const onPress = () => navigation.goBack();
+    Alert.alert(
+      '헬멧이 파손되었거나 분실되었나요?',
+      '파손 및 분실의 경우, 이용 정책에 따라 15,000원이 추가로 결제됩니다. 오류일 경우, 오류를 증명할 수 있는 사진과 함께 고객센터에 문의주시면 환불 조치해드립니다.',
+      [
+        {text: '아니요', style: 'cancel'},
+        {text: '네, 결제합니다', onPress},
       ],
       {cancelable: true},
     );
@@ -58,20 +65,24 @@ export const HelmetBorrow: React.FC = () => {
         disabled={status === 'completed'}
       />
       <Container>
-        <Title>{messages[status]}</Title>
-        <Description>
-          {status === 'completed'
-            ? '이제 헬멧을 꺼내어 착용해주세요.'
-            : '다소 시간이 걸릴 수 있습니다.'}
-        </Description>
+        <Title>헬멧을 반납해주세요.</Title>
+        <Description>{messages[status]}</Description>
         <LottieView
+          style={{marginTop: -50}}
           source={icons[status]}
           loop={status !== 'completed'}
           autoPlay
         />
+        {status !== 'completed' && (
+          <LostButton onPress={onLost}>
+            <LostButtonText>
+              혹시 헬멧을 잃어버렸거나 파손되었나요?
+            </LostButtonText>
+          </LostButton>
+        )}
       </Container>
       {status === 'completed' && (
-        <BottomButton onPress={onComplete}>이어서 라이드하기</BottomButton>
+        <BottomButton onPress={onComplete}>반납 완료</BottomButton>
       )}
     </SafeAreaView>
   );
@@ -80,6 +91,18 @@ export const HelmetBorrow: React.FC = () => {
 const Container = styled(View)`
   margin: 12% 8%;
   margin-top: ${screenHeight * 0.1}px;
+`;
+
+const LostButton = styled(TouchableOpacity)`
+  margin-top: ${screenHeight * 0.63}px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LostButtonText = styled(Text)`
+  font-size: ${screenHeight / 46}px;
+  font-weight: 900;
+  color: #696969;
 `;
 
 const Title = styled(Text)`
