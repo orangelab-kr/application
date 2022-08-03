baaabdcabf6a8870a3723e4ce6bd234a791d7f99
bakjonghun <bakjonghun@hikick.kr>
2022-08-04 02:00:59 +0900

    add: code push progress

--- src/screens/splash.tsx
+++ src/screens/splash.tsx
@@ -1,8 +1,9 @@
 import {useNavigation} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
 import {Image, Text, View} from 'react-native';
-import CodePush from 'react-native-code-push';
+import CodePush, {DownloadProgress} from 'react-native-code-push';
 import {checkMultiple, checkNotifications} from 'react-native-permissions';
+import {Bar} from 'react-native-progress';
 import styled from 'styled-components/native';
 import {screenHeight, screenWidth} from '../constants/screenSize';
 import {navigationRef} from '../navigators/navigation';
@@ -10,29 +11,66 @@ import {loginedUserState} from '../recoils/loginedUser';
 import {useRecoilValueMaybe} from '../tools/recoil';
 import {requiredPermissions} from './permission';
 
-type StatusType = 'starting' | 'checking' | 'updating';
+type StatusType =
+  | 'starting'
+  | 'checking'
+  | 'downloading'
+  | 'installing'
+  | 'restarting';
 export const StatusMessage: {
   [K in StatusType]: string;
 } = {
-  starting: '어플리케이션 실행 중...',
-  checking: '업데이트 확인 중...',
-  updating: '업데이트 중...',
+  starting: '앱을 시작하고 있습니다.',
+  checking: '업데이트를 확인하고 있습니다.',
+  downloading: '업데이트를 다운로드 받고 있습니다.',
+  installing: '업데이트를 진행하고 있습니다.',
+  restarting: '업데이트가 완료되어 앱을 재시작합니다.',
 };
 
 export const Splash: React.FC = () => {
   const user = useRecoilValueMaybe(loginedUserState);
   const [status, setStatus] = useState<StatusType>('starting');
+  const [progress, setProgress] = useState<number>();
   const navigation = useNavigation();
 
   const onVersionCheck = async () => {
-    setStatus('checking');
-    const hasUpdate = await CodePush.checkForUpdate();
-    if (!hasUpdate) return onReady();
-    setStatus('updating');
+    const options = {
+      installMode: CodePush.InstallMode.ON_NEXT_RESTART,
+      mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+      rollbackRetryOptions: {
+        delayInHours: 24,
+        maxRetryAttempts: 3,
+      },
+    };
 
-    await CodePush.sync({
-      installMode: CodePush.InstallMode.IMMEDIATE,
-    });
+    const syncStatusChangedCallback = (status: CodePush.SyncStatus) => {
+      switch (status) {
+        case CodePush.SyncStatus.UP_TO_DATE:
+          onReady();
+          break;
+        case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+          setStatus('checking');
+          break;
+        case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+          setStatus('downloading');
+          break;
+        case CodePush.SyncStatus.INSTALLING_UPDATE:
+          setStatus('installing');
+          break;
+      }
+    };
+
+    const downloadProgressCallback = ({
+      receivedBytes,
+      totalBytes,
+    }: DownloadProgress) =>
+      setProgress(Math.ceil((receivedBytes / totalBytes) * 100));
+
+    await CodePush.sync(
+      options,
+      syncStatusChangedCallback,
+      downloadProgressCallback,
+    );
   };
 
   const onReady = async () => {
@@ -48,6 +86,8 @@ export const Splash: React.FC = () => {
 
     if (user === undefined) return;
     if (user == null) return navigationRef.current?.navigate('Start');
+
+    CodePush.notifyAppReady();
     navigationRef.current?.navigate('Main');
   };
 
@@ -59,10 +99,15 @@ export const Splash: React.FC = () => {
   return (
     <View style={{flex: 1}}>
       <Container>
-        <Logo source={require('../assets/logo.png')} />
         <Description>이동을 즐겁게,</Description>
+        <Logo source={require('../assets/logo.png')} />
       </Container>
-      <ProgressMessage>{StatusMessage[status]}</ProgressMessage>
+      <ProgressContainer>
+        {progress !== undefined && (
+          <Bar progress={progress} height={4} width={180} color="#999" />
+        )}
+        <ProgressMessage>{StatusMessage[status]}</ProgressMessage>
+      </ProgressContainer>
     </View>
   );
 };
@@ -78,9 +123,9 @@ const Logo = styled(Image)`
 `;
 
 const Description = styled(Text)`
-  margin-top: 5px;
-  font-size: 20px;
-  font-weight: 400;
+  margin-bottom: -100px;
+  font-size: 18px;
+  font-weight: 700;
   shadow-color: #999;
   shadow-opacity: 0.3;
   shadow-radius: 3px;
@@ -88,10 +133,16 @@ const Description = styled(Text)`
   shadow-offset: {height: 3px, width: 3px};
 `;
 
-const ProgressMessage = styled(Text)`
+const ProgressContainer = styled(View)`
   position: absolute;
-  text-align: center;
+  align-items: center;
   width: 100%;
   bottom: 20px;
+  color: #999;
+`;
+
+const ProgressMessage = styled(Text)`
+  margin-top: 6px;
+  text-align: center;
   color: #999;
 `;
