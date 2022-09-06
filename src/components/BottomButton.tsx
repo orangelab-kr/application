c6e2481cd2278596dce42e30ed284a2c7f87a325
bakjonghun <bakjonghun@hikick.kr>
2022-09-06 18:58:03 +0900

    add: bottom button make it pretty

--- src/components/BottomButton.tsx
+++ src/components/BottomButton.tsx
@@ -1,20 +1,27 @@
-import React, { FC } from 'react';
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import React, {FC} from 'react';
+import {Text, TouchableOpacity, TouchableOpacityProps} from 'react-native';
 import styled from 'styled-components/native';
-import { screenHeight } from '../constants/screenSize';
+import {screenHeight} from '../constants/screenSize';
 
-export interface BlockButtonProps extends TouchableOpacityProps {}
+export interface BlockButtonProps extends TouchableOpacityProps {
+  backgroundColor?: string;
+  color?: string;
+}
 
-export const BottomButton: FC<BlockButtonProps> = ({children, ...props}) => {
+export const BottomButton: FC<BlockButtonProps> = ({
+  backgroundColor = '#0a0c0c',
+  color = '#fcfeff',
+  children,
+  ...props
+}) => {
   return (
-    <Button {...props}>
-      <ButtonText>{children}</ButtonText>
+    <Button {...props} style={{backgroundColor}}>
+      <ButtonText style={{color}}>{children}</ButtonText>
     </Button>
   );
 };
 
 const ButtonText = styled(Text)`
-  color: #fcfeff;
   font-weight: 600;
   font-size: ${screenHeight / 40}px;
   margin-bottom: ${screenHeight * 0.015}px;
@@ -23,7 +30,6 @@ const ButtonText = styled(Text)`
 const Button = styled(TouchableOpacity)`
   width: 100%;
   height: ${screenHeight / 11}px;
-  background-color: #0a0c0c;
   justify-content: center;
   align-items: center;
   position: absolute;
