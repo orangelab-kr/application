9b49f99314b01b84e94429fddf4f1ea6d041dda9
bakjonghun <bakjonghun@hikick.kr>
2022-09-26 15:19:05 +0900

    update: many packages (changing camera module)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import CodePush from 'react-native-code-push';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
+import 'react-native-reanimated';
 import {name as appName} from './app.json';
 import './shim';
 import {App} from './src/App';
