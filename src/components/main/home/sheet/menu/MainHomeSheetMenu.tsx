92bf10107821ce8d0164ed985b32b6ac56eb0244
bakjonghun <bakjonghun@hikick.kr>
2022-12-05 18:03:55 +0900

    fix: build failed

--- src/components/main/home/sheet/menu/MainHomeSheetMenu.tsx
+++ src/components/main/home/sheet/menu/MainHomeSheetMenu.tsx
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react';
-import RNShake from 'react-native-shake';
 import {useRecoilValue, useSetRecoilState} from 'recoil';
-import {navigationRef} from '../../../../../navigators/navigation';
-import {menuPopupState} from '../../../../../recoils/menuPopup';
-import {modeState} from '../../../../../recoils/mode';
+
 import {MainHomeSheetMenuButton} from './MainHomeSheetMenuButton';
 import {MainHomeSheetMenuPopup} from './MainHomeSheetMenuPopup';
+import RNShake from 'react-native-shake';
+import {menuPopupState} from '../../../../../recoils/menuPopup';
+import {modeState} from '../../../../../recoils/mode';
+import {navigationRef} from '../../../../../navigators/navigation';
 
 export const MainHomeSheetMenu: React.FC = () => {
   const mode = useRecoilValue(modeState);
